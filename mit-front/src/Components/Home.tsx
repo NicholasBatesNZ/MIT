5ad74c5448033cbe9a689db0e2cc5e0781d9f0f1
NicholasBatesNZ <NicholasBatesNZ@users.noreply.github.com>
2021-07-08 12:07:21 +1200

    fix: :bug: only notify logged in users (on home screen)

--- mit-front/src/Components/Home.tsx
+++ mit-front/src/Components/Home.tsx
@@ -2,11 +2,12 @@ import { useLocation } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import { HistoryPropType } from '../types';
 
-const socket = io('ws://localhost:3001');
-socket.on('message', (message) => alert(message));
-
 export default function Home(props: HistoryPropType): JSX.Element {
+    const socket = io('ws://localhost:3001');
+    socket.on('message', (message) => alert(message));
+
     const location = useLocation<{ authenticated: boolean }>();
     if (!location.state?.authenticated) props.history.push('/');
+    
     return <button onClick={() => socket.send('Hello Friends')}>Send</button>;
 }
