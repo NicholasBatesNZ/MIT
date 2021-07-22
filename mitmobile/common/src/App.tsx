698504233295aee91440c9dd2def2c3bc541445b
NicholasBatesNZ <NicholasBatesNZ@users.noreply.github.com>
2021-07-23 10:51:52 +1200

    feat: :construction: setup firebase and handle test messages

--- mitmobile/common/src/App.tsx
+++ mitmobile/common/src/App.tsx
@@ -1,4 +1,6 @@
+import messaging from '@react-native-firebase/messaging';
 import React from 'react';
+import { Alert } from 'react-native';
 import { BackButton, NativeRouter, Route, Switch } from 'react-router-native';
 import Home from './Components/Home';
 import Landing from './Components/Landing';
@@ -6,6 +8,9 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 
 export default function App(): JSX.Element {
+    messaging().onMessage(async message => Alert.alert('You have a message!', JSON.stringify(message)));
+    messaging().setBackgroundMessageHandler(async message => console.log(`Message received in background: ${JSON.stringify(message)}`));
+
     return (
         <NativeRouter>
             <BackButton>
